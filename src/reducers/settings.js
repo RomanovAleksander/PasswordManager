d71619f07db46c2789380c7e8ceb2b8eef59f22a
RomanovAleksander <1eksandr999@gmail.com>
2021-06-02 19:36:13 +0300

    settings reducer updated

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -9,17 +9,33 @@ const initialState = {
   isUA: false
 };
 
+const isDarkTrue = (isDark) => {
+  if (isDark === true) {
+    return true;
+  } else if (isDark === false) {
+    return false;
+  } else {
+    return initialState.isDark;
+  }
+};
+
+const isUATrue = (isUA) => {
+  if (isUA === true) {
+    return true
+  } else if (isUA === false) {
+    return false;
+  } else {
+    return initialState.isUA;
+  }
+};
+
 export const settings = (state, action) => {
   if (state === undefined) {
-    if (localStorage.isDark) {
+    if (localStorage.isDark || localStorage.isUA) {
       return {
         ...initialState,
-        isDark: LocalStorageService.getItem('isDark')
-      }
-    }  else if (localStorage.isUA) {
-      return {
-        ...initialState,
-        isUA: LocalStorageService.getItem('isUA')
+        isDark: isDarkTrue(LocalStorageService.getItem('isDark')),
+        isUA: isUATrue(LocalStorageService.getItem('isUA'))
       }
     } else {
     return initialState
